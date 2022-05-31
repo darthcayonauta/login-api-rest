60699ec3038df86af9f005643d7bfa4c25198a62
darthcayonauta <claudio.guzman@socma.cl>
2022-05-31 09:05:34 -0400

    boton en espera

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,57 +1,50 @@
 import React from "react";
 import "../css/login.css";
-import {useRef,useState} from 'react';
+import { useRef, useState } from "react";
 
-const URL_LOGIN = 'http://localhost/ws-login/login.php';
-
+const URL_LOGIN = "http://localhost/ws-login/login.php";
 
 //send data to backend
-const enviarData = async ( url,data )=>{
-    const resp = await fetch(url,{ 
-                            method:'POST',
-                            body: JSON.stringify(data),
-                            headers:{
-                                'Content-Type': 'application/json'
-                            }
-    });
-         //console.log(resp);
-         const json = await resp.json();
-         //console.log(json);
+const enviarData = async (url, data) => {
+  const resp = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  //console.log(resp);
+  const json = await resp.json();
+  //console.log(json);
 
-         return json;
-    }
-
-
+  return json;
+};
 
 function Login(props) {
+  const [error, setError] = useState(null);
+  const [espera, setEspera] = useState(false);
 
-    const [error,setError] = useState(null);
-    const [espera,setEspera] = useState(false);
+  const refUsuario = useRef(null);
+  const refClave = useRef(null);
 
-    const refUsuario    = useRef(null);
-    const refClave      = useRef(null);
+  //captura data del formulario
+  const handleLogin = async () => {
+    setEspera(true);
 
+    const usuario = refUsuario.current.value;
+    const clave = refClave.current.value;
 
-    
-    //captura data del formulario
-    const handleLogin = async ()=>{
-          
-        setEspera(true);
+    const data = { usuario: usuario, clave: clave };
+    const respuestaJson = await enviarData(URL_LOGIN, data);
 
-        const usuario = refUsuario.current.value;
-        const clave   = refClave.current.value;
+    console.log("respuesta:", respuestaJson.conectado);
 
-        const data = { 'usuario': usuario, 'clave':clave };
-        const respuestaJson =  await enviarData(URL_LOGIN,data)
+    props.acceder(respuestaJson.conectado);
+    setError(respuestaJson.error);
+    setEspera(false);
+  };
 
-        console.log("respuesta:",respuestaJson.conectado);
-
-        props.acceder( respuestaJson.conectado );
-        setError(respuestaJson.error);
-        setEspera(false);
-    }
-  
-    return (
+  return (
     <div className="login">
       <div className="row">
         <div className="col-sm-4 offset-4 mt-5">
@@ -91,18 +84,15 @@ function Login(props) {
                 />
               </div>
 
-              {
-                  error &&
-                  <div className='alert alert-danger'>
-                  {error}
-                </div> 
-              }  
-         
+              {error && <div className="alert alert-danger">{error}</div>}
 
-              <button 
-                onClick = {handleLogin}
-                disabled = {espera}
-                className="btn btn-info btn-lg btn-block">Acceder</button>
+              <button
+                onClick={handleLogin}
+                disabled={espera}
+                className="btn btn-info btn-lg btn-block"
+              >
+                Acceder
+              </button>
               <div className="card-footer text-center">
                 <span> ¿Olvidó su clave?</span> <a href="#">Recuperar</a>
               </div>
